0479af3f5a30ed9ced06e57583c2c802e02bcab9
Oleksandra Shergina <sasha.shergina@gmail.com>
2023-12-05 00:19:46 +0200

    first function

---
+++ diskretka.py
@@ -0,0 +1,35 @@
+'''Комп'ютерний проєкт'''
+
+# якщо треба словник та Dict=True, якщо неорієнтований граф то Oriented=False
+def read_graph(file, Dict=False, Oriented=True):
+    '''
+    Reads a csv and returns a graph in the form of a list or dictionary.
+    '''
+    with open(file, mode='r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for i,l in enumerate(lines):
+            lines[i] = [int(i) for i in l.rstrip().split(',')]
+    list_graph = [(lines[0][i], lines[1][i]) for i in range(len(lines[0]))]
+    if not Dict:
+        list_graph = [(lines[0][i], lines[1][i]) for i in range(len(lines[0]))]
+        if not Oriented:
+            for x in list_graph:
+                for y in list_graph:
+                    if y == (x[1],x[0]) and x[0] != x[1]:
+                        list_graph.pop(list_graph.index(y))
+                        continue
+        return list_graph
+    else:
+        if Oriented:
+            res = {i[0]:{k[1] for k in list_graph if k[0]==i[0]} for i in list_graph}
+        else:
+            res = {}
+            for y in set(lines[0] + lines[1]):
+                points = set()
+                for x in list_graph:
+                    if y == x[0]:
+                        points.add(x[1])
+                    elif y == x[1]:
+                        points.add(x[0])
+                res[y] = points
+        return res
