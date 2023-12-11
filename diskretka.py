edcb5358dd14c794f80e6d97bc307ac2c74ae5bc
Oleksandra Shergina <sasha.shergina@gmail.com>
2023-12-11 12:46:30 +0200

    fixed first function

--- diskretka.py
+++ diskretka.py
@@ -2,7 +2,7 @@
 
 # якщо треба словник та Dict=True, якщо неорієнтований граф то Oriented=False
 # якщо граф орієнтований, то у списку будуть тапли, якщо ні то множини
-def read_graph(file, Dict=False, Oriented=True):
+def read_graph(file):
     '''
     Reads a csv and returns a graph in the form of a list or dictionary.
     '''
@@ -11,26 +11,17 @@ def read_graph(file, Dict=False, Oriented=True):
         for i,l in enumerate(lines):
             lines[i] = [int(i) for i in l.rstrip().split(',')]
     list_graph = [(lines[0][i], lines[1][i]) for i in range(len(lines[0]))]
-    if not Dict:
-        list_graph = [(lines[0][i], lines[1][i]) for i in range(len(lines[0]))]
-        if not Oriented:
-            for i,x in enumerate(list_graph):
-                list_graph[i] = set(x)
-        return list_graph
-    else:
-        if Oriented:
-            res = {i[0]:{k[1] for k in list_graph if k[0]==i[0]} for i in list_graph}
-        else:
-            res = {}
-            for y in set(lines[0] + lines[1]):
-                points = set()
-                for x in list_graph:
-                    if y == x[0]:
-                        points.add(x[1])
-                    elif y == x[1]:
-                        points.add(x[0])
-                res[y] = points
-        return res
+    res = {}
+    all_points = set([x[0] for x in list_graph] + [y[1] for y in list_graph])
+    for x in all_points:
+        points = []
+        for y in list_graph:
+            if y[0] == x:
+                points.append(y[1])
+            elif y[1] == x:
+                points.append(y[0])
+        res[x] = points
+    return res
 
 def hamilton(graph):
     pass
@@ -44,5 +35,31 @@ def double(graph):
 def isomorph(graph1, graph2):
     pass
 
-def color(graph):
-    pass
+def graph_coloring(graph):
+    """Розфарбовування графа жадібним алгоритмом
+    >>> graph_coloring({'A': ['B', 'C'], 'B': ['A', 'C'], 'C': ['B', 'A']})
+    {'A': 'Red', 'B': 'Green', 'C': 'Blue'}
+    >>> graph_coloring({'A': ['B', 'C', 'D'], 'B': ['A', 'C'], 'C': ['A', 'B', 'D'], 'D': ['A', 'C']})
+    {'A': 'Red', 'B': 'Green', 'C': 'Blue', 'D': 'Green'}
+    >>> graph_coloring({'A': ['B', 'C'], 'B': ['A', 'C'], 'C': ['A', 'B'], 'D': ['B', 'E'], 'E': ['D']})
+    {'A': 'Red', 'B': 'Green', 'C': 'Blue', 'D': 'Red', 'E': 'Green'}
+    >>> graph_coloring({'A': ['B', 'C', 'D'], 'B': ['A', 'C', 'D'], 'C': ['A', 'B', 'D'], 'D': ['A', 'B', 'C']})
+    'Неможливо зафарбувати'
+    >>> graph_coloring({'A': ['B', 'C', 'D', 'E'], 'B': ['A', 'C', 'D', 'E'], 'C': ['A', 'B', 'D', 'E'], 'D': ['A', 'B', 'C', 'E'], 'E': ['A', 'B', 'C', 'D']})
+    'Неможливо зафарбувати'
+    """
+    def color(lst): # функція для визначення кольору
+        colors = set(lst) # всі кольори, які вже використані
+        color_lst = ['Red', "Green", "Blue"] # можливі кольори
+        for i in range(3): # перевіряємо, якого кольору немає в сусідів
+            if color_lst[i] not in colors: # якщо такого кольору немає, то повертаємо його
+                return color_lst[i]
+        return None
+    color_dict = dict() # словник, де ключ - вершина, а значення - колір
+    for node in graph: # проходимо по всіх вершинах
+        visited_color = [color_dict[elem] for elem in graph[node] if elem in color_dict]
+# перевіряємо, які кольори вже використані
+        color_dict[node] = color(visited_color) # зафарбовуємо вершину
+        if color_dict[node] is None: # якщо не вдалося зафарбувати, то повертаємо None
+            return "Неможливо зафарбувати"
+    return color_dict
